df6d457d640e1e1490170b4e96ae3f98116ec20e
deep-bansal <deepvinaybansal@yahoo.com>
2020-11-28 16:14:03 +0530

    add to favourite function

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,19 @@ class App extends React.Component {
     console.log('STATE',this.props.store.getState());
   }
 
+  isMovieFavourite = (movie) => {
+    const {favourites} = this.props.store.getState();
+
+    const index = favourites.indexOf(movie);
+
+    if(index !== -1){
+      //found the  movie
+      return true;
+    }
+    return false;
+
+  }
+
   render() {
     const {list} = this.props.store.getState();
     console.log('render',this.props.store.getState());
@@ -34,7 +47,10 @@ class App extends React.Component {
 
             <div className="list">
               {list.map((movie,index) => (
-                <MovieCard movie={movie} key = {`movies-${index}`}/>
+                <MovieCard movie={movie}
+                key = {`movies-${index}`}
+                dispatch = {this.props.store.dispatch}
+                isFavourite = {this.isMovieFavourite(movie)}/>
               ))}
             </div>
           </div>
